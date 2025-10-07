a6545ab0fc7adb4490e382517ac810b35baaaf6b
belen <noahnolasco1612@gmail.com>
2025-10-06 21:43:04 -0300

    arreglo de env y titulosDescrpciones

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -8,12 +8,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDfxTEvta3y2gbjpl7SmjbjoB3i8sTYcT0",
-  authDomain: "recetario-1a250.firebaseapp.com",
-  projectId: "recetario-1a250",
-  storageBucket: "recetario-1a250.firebasestorage.app",
-  messagingSenderId: "166420355541",
-  appId: "1:166420355541:web:48e20206e940ffb98b055c",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: "G-VKHFZH789H"
 };
 
